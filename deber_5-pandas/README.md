9e7fab6f5ffacae1d7aa214d57aec0ce89f7d7ac
angel.re.re.re <angel.re.re.re@gmail.com>
2026-08-11 20:20:08 -0500

    Deber 5 Pandas completado

---
+++ deber_5-pandas/README.md
@@ -0,0 +1,8 @@
+# ejemplo
+# Evidencias del Taller 5
+
+## Resultado de la ejecución:
+Fruta  Cantidad
+0  Manzana        10
+1     Pera         5
+2   Banano        20
---
+++ deber_5-pandas/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# 1. Crear un diccionario con datos
+datos = {'Fruta': ['Manzana', 'Pera', 'Banano'], 'Cantidad': [10, 5, 20]}
+
+# 2. Crear la tabla (DataFrame)
+df = pd.DataFrame(datos)
+
+# 3. Mostrar la tabla
+print(df)
